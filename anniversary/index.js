0a0b55698f61f4945f3bc7eab1f125b4ae714b79
LJP-Perfect <13106058519@163.com>
2020-04-13 12:49:17 +0800

    Site updated: 2020-04-13 12:49:05

--- anniversary/index.js
+++ anniversary/index.js
@@ -1,5 +1,5 @@
-document.querySelector('.chat[data-chat=record0411]').classList.add('active-chat');
-document.querySelector('.person[data-chat=record0411]').classList.add('active');
+document.querySelector('.chat[data-chat=record0413]').classList.add('active-chat');
+document.querySelector('.person[data-chat=record0413]').classList.add('active');
 
 var friends = {
   list: document.querySelector('ul.people'),
